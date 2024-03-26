d3c94e86b2330fcc532782f162a7b660a60f5d26
jrunge <jrunge@precisionplanting.com>
2024-03-26 00:02:06 -0500

    Working on basic structure

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,25 @@
 use std::io::{self, stdout};
-
 use crossterm::{
     event::{self, Event, KeyCode},
     terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
     ExecutableCommand,
 };
+use ratatui::prelude::*;
 
-use ratatui::{prelude::*, widgets::*};
+mod views {
+    pub mod intro;
+}
+
+mod defs;
 
 fn main()-> io::Result<()> {
     enable_raw_mode()?;
     stdout().execute(EnterAlternateScreen)?;
     let mut terminal = Terminal::new(CrosstermBackend::new(stdout()))?;
+
+    let views: Vec<defs::View> = vec![
+        defs::View::new(defs::ViewType::Intro, None, true),
+    ];
 
     let mut should_quit = false;
     while !should_quit {
@@ -37,11 +45,43 @@ fn handle_events()-> io::Result<bool> {
 }
 
 fn ui(frame: &mut Frame) {
-    frame.render_widget(
-        Paragraph::new("Hello World!")
-            .block(Block::default()
-            .title("Greeting")
-            .borders(Borders::ALL)),
-        frame.size(),
-    );
+
+    // frame.render_widget(
+    //     views::intro::render(),
+    //     frame.size(),
+    // );
+    // let main_layout = Layout::new(
+    //     Direction::Vertical,
+    //     vec![
+    //         Constraint::Length(1),
+    //         Constraint::Min(0),
+    //         Constraint::Length(1),
+    //     ],
+    // )
+    // .split(frame.size());
+
+    // frame.render_widget(
+    //     Block::new().borders(Borders::TOP).title("Title bar"),
+    //     main_layout[0],
+    // );
+
+    // frame.render_widget(
+    //     Block::new().borders(Borders::TOP).title("Status bar"),
+    //     main_layout[2],
+    // );
+
+    // let inner_layout = Layout::new(
+    //     Direction::Horizontal,
+    //     vec![Constraint::Percentage(50), Constraint::Percentage(50)],
+    // )
+    // .split(main_layout[1]);
+
+    // frame.render_widget(
+    //     Block::default().borders(Borders::ALL).title("Left"),
+    //     inner_layout[0],
+    // );
+    // frame.render_widget(
+    //     Block::default().borders(Borders::ALL).title("Right"),
+    //     inner_layout[1],
+    // );
 }
